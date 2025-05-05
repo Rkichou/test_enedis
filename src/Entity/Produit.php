7c2ee7eb535e2dda41200c84b0ec95023ca1cca9
Rkichou <156240874+Rkichou@users.noreply.github.com>
2025-05-05 15:00:38 +0200

    amelioration de l'interface utilisateur

--- src/Entity/Produit.php
+++ src/Entity/Produit.php
@@ -14,7 +14,7 @@ class Produit
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
-
+#[Assert\NotBlank()]
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $nom = null;
 
